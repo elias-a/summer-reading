036e6177b978df1bcbf5c8c01d336c351ecea043
elias-a <elias.alstead@gmail.com>
2022-06-22 22:51:53 -0400

    Display spinner and error message on auth screen

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,15 @@
-import { createSignal, createResource } from 'solid-js';
-import { authenticate } from '../api/users';
+import { createSignal } from 'solid-js';
 import AuthScreen from './AuthScreen';
 import Header from './Header';
 import Library from './Library';
 import styles from './App.module.css';
 
 const App = () => {
-  const [password, setPassword] = createSignal('');
-  const [user] = createResource(password, authenticate);
+  const [user, setUser] = createSignal(null);
 
   return (
     <div class={styles.App}>
-      <Show when={user()} fallback={<AuthScreen setPassword={setPassword} />}>
+      <Show when={user()} fallback={<AuthScreen setUser={setUser} />}>
         <>
           <Header user={user()} />
           <Library />
--- client/src/components/AuthScreen.jsx
+++ client/src/components/AuthScreen.jsx
@@ -1,33 +1,69 @@
 import { createSignal } from 'solid-js';
 import { BsArrowRightCircle } from 'solid-icons/bs';
+import { authenticate } from '../api/users';
 import styles from './AuthScreen.module.css';
 
 const AuthScreen = (props) => {
   const [password, setPassword] = createSignal('');
-  
+  const [isSubmitting, setIsSubmitting] = createSignal(false);
+  const [error, setError] = createSignal(false);
+
+  const updatePassword = (event) => {
+    setPassword(event.currentTarget.value);
+    setError(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    setIsSubmitting(true);
+    authenticate(password()).then(user => {
+      if (user) {
+        props.setUser(user);
+      } else {
+        setError(true);
+      }
+
+      setIsSubmitting(false);
+    });
+  };
+
   return (
     <div class={styles.AuthScreen}>
       <div class={styles.modal}>
-        <div class={styles.labelSection}>
-          <label for="password" class={styles.label}>Enter Password:</label>
-        </div>
-        <div class={styles.inputSection}>
-          <input
-            id="password"
-            class={styles.input}
-            type="password"
-            tabIndex="1"
-            value={password()}
-            onInput={e => setPassword(e.currentTarget.value)}
-          />
-        </div>
-        <div class={styles.submitSection}>
-          <BsArrowRightCircle
-            size={32}
-            color="#000000"
-            onClick={() => props.setPassword(password())}
-          />
-        </div>
+        <form class={styles.form} onSubmit={handleSubmit}>
+          <div class={styles.labelSection}>
+            <label for="password" class={styles.label}>Enter Password:</label>
+          </div>
+          <div class={styles.inputSection}>
+            <div>
+              <input
+                id="password"
+                class={`${error() ? styles.error : ''}`}
+                type="password"
+                autoComplete="password"
+                value={password()}
+                onInput={e => updatePassword(e)}
+              />
+              <Show when={error()}>
+                <p class={styles.errorMessage}>Incorrect password</p>
+              </Show>
+            </div>
+          </div>
+          <div class={styles.submitSection}>
+            <Show
+              when={!isSubmitting()}
+              fallback={<div class={styles.loader} />}
+            >
+              <BsArrowRightCircle
+                size={32}
+                color="#000"
+                onClick={handleSubmit}
+                class={styles.submitButton}
+              />
+            </Show>
+          </div>
+        </form>
       </div>
     </div>
   );
